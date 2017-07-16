35a3c2f1fe0589cbea0eef07ccc50159d95df12d
hanwjdgh <hanwjdgh@naver.com>
2017-07-17 08:51:11 +0900

    10869

---
+++ 10869.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(void) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	printf("%d\n", (a + b));
+	printf("%d\n", (a - b));
+	printf("%d\n", (a * b));
+	printf("%d\n", (a / b));
+	printf("%d\n", (a % b));
+	return 0;
+}
