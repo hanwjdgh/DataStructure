f84de52184188028e6b6d9f707645cb77e799dc6
hanwjdgh <hanwjdgh@naver.com>
2017-07-20 19:56:00 +0900

    2741

--- 2741.cpp
+++ 2741.cpp
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include <cstdio>
 
-
-int main(void){
+int main(void) {
 	int N;
 	int a;
 	scanf("%d", &N);
 	for (a = 1; a <= N; a++)
-		printf("%d\n",a);
+		printf("%d\n", a);
 	return 0;
 }
