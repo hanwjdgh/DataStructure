da677b6a6ea686feb6eb3c9183c9d33035689f86
hanwjdgh <hanwjdgh@naver.com>
2017-08-22 13:01:27 +0900

    1004

--- 1003.cpp
+++ 1003.cpp
@@ -1,28 +1,10 @@
 #include <cstdio>
+using namespace std;
 
-int a, b;
-
-int fibonacci(int n) {
-	if (n == 0) {
-		++a;
-		return 0;
-	}
-	else if (n == 1) {
-		++b;
-		return 1;
-	}
-	else {
-		return fibonacci(n - 1) + fibonacci(n - 2);
-	}
-}
 int main(void) {
-	int num, x, test;
-	scanf("%d", &num);
-	for (x = 0; x < num; ++x) {
-		a = b = 0;
-		scanf("%d", &test);
-		fibonacci(test);
-		printf("%d %d\n", a, b);
-	}
+	int num, f_size, c_size;
+	scanf("%d", &num); getchar();
+	scanf("%d", &f_size); getchar();
+	scanf("%d", &c_size);
 	return 0;
 }
--- 1004.cpp
+++ 1004.cpp
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <math.h>
+#include <cstdio>
+#include <cmath>
+using namespace std;
 
-int main(void){
-	int numofcase,cnt;
-	int a,numofp,b;
+int main(void) {
+	int numofcase, cnt;
+	int a, numofp, b;
 	int x1, y1, x2, y2;
 	int px, py, pr;
-	double dist1,dist2;
+	double dist1, dist2;
 	scanf("%d", &numofcase);
-	for (a = 0; a < numofcase; ++a){
+	for (a = 0; a < numofcase; ++a) {
 		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 		scanf("%d", &numofp);
 		cnt = 0;
-		for (b = 0; b < numofp; ++b){
+		for (b = 0; b < numofp; ++b) {
 			scanf("%d %d %d", &px, &py, &pr);
 			dist1 = sqrt((px - x1)*(px - x1) + (py - y1)*(py - y1));
 			dist2 = sqrt((px - x2)*(px - x2) + (py - y2)*(py - y2));
