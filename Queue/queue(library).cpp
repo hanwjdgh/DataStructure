67a1a480ad03d0d01f557b5355e39d25eeba502f
hanwjdgh <hanwjdgh@naver.com>
2018-06-26 19:25:56 +0900

    Queue(library)

--- Queue/queue(library).cpp
+++ Queue/queue(library).cpp
@@ -5,18 +5,19 @@ using namespace std;
 
 int main() {
 	queue<int> q;
-	
+
 	for (int i = 0; i < 5; i++)
 		q.push(i);
-
-	cout <<q.front() << endl;
-	cout <<q.back() << endl;
-	cout <<q.size() << endl;
-	cout <<(q.empty() ? "Yes" : "No") << endl;
+	// 0 1 2 3 4
+	cout << q.front() << endl; // 0
+	cout << q.back() << endl; // 4
+	cout << q.size() << endl; // 5
+	cout << (q.empty() ? "Yes" : "No") << endl; // No
 
 	while (!q.empty()) {
 		cout << q.front() << " ";
 		q.pop();
 	}
+	//0 1 2 3 4
 	return 0;
 }
