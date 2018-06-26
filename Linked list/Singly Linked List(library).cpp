b54a27f434fcf9c2d95fca79c34c4c53b4c0aa35
hanwjdgh <hanwjdgh@naver.com>
2018-06-26 15:50:29 +0900

    Singly Linked List(library)

--- Linked list/Singly Linked List(library).cpp
+++ Linked list/Singly Linked List(library).cpp
@@ -3,17 +3,31 @@
 
 using namespace std;
 
-int main(){
-	list<int> mylist;
-	list<int>::iterator it;
+list<int> mylist;
 
+void print() {
+	list<int>::iterator it;
+	for (it = mylist.begin(); it != mylist.end(); it++)
+		printf("%d ", *it);
+	printf("\n");
+}
+
+int main() {
 	for (int i = 0; i < 5; i++)
 		mylist.push_back(i);
+	print(); // 0 1 2 3 4
+	printf("%d\n", mylist.size()); // 5
+	printf("%s\n",(mylist.empty() ? "Yes" : "No")); //No
 
-	for (it = mylist.begin(); it != mylist.end(); it++) 
-		printf("%d ", *it);
-	
-	printf("%d", mylist.size());
+	mylist.push_front(-1); print(); // -1 0 1 2 3 4
+
+	list<int>::iterator point = mylist.begin();
+	point++; point++;
+	mylist.insert(point,8); print(); // -1 0 8 1 2 3 4
+	mylist.pop_front(); print(); // 0 8 1 2 3 4
+	mylist.pop_back(); print(); // 0 8 1 2 3
+	point--;
+	mylist.erase(point); print(); // 0 1 2 3
 	mylist.clear();
 	return 0;
 }
