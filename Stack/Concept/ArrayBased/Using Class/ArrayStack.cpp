ad59b573e7da902e140fc1fe0846f977ecbac0bc
hanwjdgh <hanwjdgh@naver.com>
2018-11-19 14:24:47 +0900

    Stack concept using class

--- Stack/Concept/ArrayBased/Using Class/ArrayStack.cpp
+++ Stack/Concept/ArrayBased/Using Class/ArrayStack.cpp
@@ -3,110 +3,116 @@
 
 using namespace std;
 
-// Node class
-class Node {
-public:
-	int data;
-    
-	Node(int data);
-	~Node();
-};
-
-// Constructor
-Node::Node(int data) {
-	this->data = data;
-}
-// Destructor 
-Node::~Node() {
-	cout << "Deleting node (" << this->data << ")" << "\n";
-}
-
-
 // Stack classs
 class Stack {
+private:
+	int STACK_LEN;
+	int topIndex;
+	int *stackArr;
 public:
-	int size;
-	int top;
-	Node** nodes;
-
-	Stack(int size);
-	~Stack();
-
-	void Push(int data);
-	int Pop();
-	bool IsEmpty();
-	int GetSize();
-	int Peek();
-};
-
-// Constructor
-Stack::Stack(int size) {
-	this->nodes = new Node*[size];
-	this->size = size;
-	this->top = 0;
-}
-// Destructor 
-Stack::~Stack() {
-	for (int i = 0; i < this->size; i++) {
-		if (this->nodes[i]) {
-			delete this->nodes[i];
-		}
+	// Constructor
+	Stack(int size) {
+		topIndex = -1;
+		STACK_LEN = size;
+		stackArr = new int[size];
 	}
-	delete[] this->nodes;
-}
+	// Destructor 
+	~Stack() {
+		cout << "delete : ";
+		for (int i = topIndex; i > -1; i--) {
+			cout << stackArr[i] << " ";
+		}
+		cout << "\n";
+		delete[] stackArr;
+	}
 
-// Insert the element to top
-void Stack::Push(int data) {
-	this->nodes[this->top++] = new Node(data);
-	cout << "Push: " << data << "\n";
-}
-// Delete the element in top
-int Stack::Pop() {
-	int data = this->nodes[--(this->top)]->data;
-	cout << "Pop: " << data << "\n";
-	return data;
-}
-// Return true if the stack is empty or false otherwise
-bool Stack::IsEmpty() {
-	return (this->top == 0);
-}
-// Return size of stack
-int Stack::GetSize() {
-	return this->top;
-}
-// Return the element at the top
-int Stack::Peek() {
-	return this->nodes[this->top - 1]->data;
-}
+	// Push data
+	void push(int data) {
+		// If stack is full, return
+		if (isFull()) {
+			cout << "Stack is Full \n";
+			return;
+		}
+		cout << "Push : " << data << "\n";
+
+		// Push data to stack
+		stackArr[++topIndex] = data;
+	}
+	// Pop data
+	void pop() {
+		// If stack is empty, return
+		if (isEmpty()) {
+			cout << "Stack is Empty \n";
+			return;
+		}
+
+		// Pop data
+		cout << "Pop : " << stackArr[topIndex--] << "\n";
+	}
+	// Return true if the stack is full or false otherwise
+	bool isFull() {
+		return topIndex + 1 == STACK_LEN;
+	}
+	// Return true if the stack is empty or false otherwise
+	bool isEmpty() {
+		return topIndex == -1;
+	}
+	// Return size of stack
+	int getSize() {
+		return topIndex + 1;
+	}
+	// Return the data at the top
+	void peek() {
+		if (isEmpty()) {
+			cout << "Stack is Empty \n";
+			return;
+		}
+		cout << "Peek: " << stackArr[topIndex - 1] << "\n";
+	}
+	// Print stack from top to bottom
+	void print() {
+		// If stack is empty, return
+		if (isEmpty()) {
+			cout << "Stack is Empty \n";
+			return;
+		}
+
+		// Print stack from top to bottom
+		cout << "Top ";
+		for (int i = topIndex; i > -1; i--)
+			cout << stackArr[i] << " ";
+		cout << "Bottom \n";
+	}
+};
 
 int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-    // Declaration array stack with size thirty
+	// Declaration array stack with size
 	Stack stack(3);
 
-    // Push data in stack
-	stack.Push(1);
-	stack.Push(2);
-	stack.Push(3);
+	// Push data in stack
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+	stack.push(4);
 
-    // Print data that top of stack
-	cout << "Peek: " << stack.Peek() << "\n";
-    // Print size of stack
-	cout << "Size: " << stack.GetSize() << "\n";
+	// Print stack
+	stack.print();
 
-    // Pop data in stack
-	stack.Pop();
-	stack.Pop();
+	// Print data that top of stack
+	stack.peek();
+	// Print size of stack
+	cout << "Size: " << stack.getSize() << "\n";
 
-    // Print whether stack is empty or not
-	cout << "Empty: " << (stack.IsEmpty() ? "TRUE" : "FALSE") << "\n";
+	// Pop data in stack
+	stack.pop();
+	stack.pop();
 
-	stack.Pop();
-
-	cout << "Empty: " << (stack.IsEmpty() ? "TRUE" : "FALSE") << "\n";
+	// Print whether stack is empty or not
+	cout << "Empty: " << (stack.isEmpty() ? "TRUE" : "FALSE") << "\n";
 
 	return 0;
 }
