0663a2ccfbb815f85287e59960d2e487f8a4ebd7
hanwjdgh <hanwjdgh@naver.com>
2018-11-19 15:01:46 +0900

    Queue concept using header

--- Queue/Concept/Queue/LinkedListBased/Using Header/LinkedListQueue.h
+++ Queue/Concept/Queue/LinkedListBased/Using Header/LinkedListQueue.h
@@ -18,7 +18,7 @@ void init(LQueue *pqueue); // Queue initialization
 bool isEmpty(LQueue *pqueue); // Return true if the queue is empty or false otherwise
 void enQueue(LQueue *pqueue, int value); //Insert the element to queue
 void deQueue(LQueue *pqueue); // Delete the element in queue
-int getSize(LQueue *pqueue); // Return size of ququq
+int getSize(LQueue *pqueue); // Return size of quque
 void peek(LQueue *pqueue); // Return the element at the front
 void print(LQueue *pqueue); // Print queue
 
