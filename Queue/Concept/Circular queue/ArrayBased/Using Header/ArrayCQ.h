362730ee3990a520026ccdafec14f2f38e55b8c0
hanwjdgh <hanwjdgh@naver.com>
2018-11-24 14:28:56 +0900

    Add comment

--- Queue/Concept/Circular queue/ArrayBased/Using Header/ArrayCQ.h
+++ Queue/Concept/Circular queue/ArrayBased/Using Header/ArrayCQ.h
@@ -1,8 +1,9 @@
 #ifndef __A_STACK_H_
 #define __A_STACK_H_
 
-#define CQ_LEN 3 
+#define CQ_LEN 3 // Circular queue length
 
+// Circular Queue node
 typedef struct arrayCircularQueue {
 	int cqArr[CQ_LEN];
 	int front,rear;
@@ -10,13 +11,13 @@ typedef struct arrayCircularQueue {
 
 typedef ArrayCircularStack CQueue;
 
-void init(CQueue *pstack); 
-bool isFull(CQueue *pstack); 
-bool isEmpty(CQueue *pstack); 
-void enQueue(CQueue *pstack, int data); 
-void deQueue(CQueue *pstack);
-int getSize(CQueue *pstack); 
-void peek(CQueue *pstack); 
-void print(CQueue *pstack); 
+void init(CQueue *pstack); // Circular queue initialization
+bool isFull(CQueue *pstack); // Return true if the circular queue is full or false otherwise
+bool isEmpty(CQueue *pstack); // Return true if the circular queue is empty or false otherwise
+void enQueue(CQueue *pstack, int data); // Push data
+void deQueue(CQueue *pstack); // Pop data
+int getSize(CQueue *pstack);  // Return size of circular queue
+void peek(CQueue *pstack);  // Print the element at the of circular queue
+void print(CQueue *pstack); // Print circular queue
 
 #endif
--- Queue/Concept/Circular queue/ArrayBased/Using Header/main.cpp
+++ Queue/Concept/Circular queue/ArrayBased/Using Header/main.cpp
@@ -9,28 +9,38 @@ int main() {
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
+	// Declararation Array circular queue
 	CQueue cqueue;
 
+	// Initialize circular queue
 	init(&cqueue);
 
+	// Push data
 	enQueue(&cqueue, 1);
 	enQueue(&cqueue, 2);
 	enQueue(&cqueue, 3);
 
+	// Print circular queue from front to rear
 	print(&cqueue);
 
+	// Print data that front of circuluar queue
 	peek(&cqueue);
+	// Print size of circuluar queue
 	cout << "Size: " << getSize(&cqueue) << "\n";
 
+	// Pop data
 	deQueue(&cqueue);
 	deQueue(&cqueue);
 
+	// Print whether circular queue is empty or not
 	cout << "Empty: " << (isEmpty(&cqueue) ? "TRUE" : "FALSE") << "\n";
 
+	// Push data
 	enQueue(&cqueue,4);
 	enQueue(&cqueue,5);
 	enQueue(&cqueue,6);
 
+	// Print data
 	print(&cqueue);
 
 	return 0;
