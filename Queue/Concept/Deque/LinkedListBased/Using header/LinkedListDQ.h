63ac1b0113283ca2b55b37dd110ad278fa316607
hanwjdgh <hanwjdgh@naver.com>
2018-11-25 15:33:33 +0900

    Add functions

--- Queue/Concept/Deque/LinkedListBased/Using header/LinkedListDQ.h
+++ Queue/Concept/Deque/LinkedListBased/Using header/LinkedListDQ.h
@@ -12,6 +12,14 @@ typedef struct listDQueue{
 
 typedef ListDQueue DQueue;
 
+void init(DQueue *pqueue);
+bool isEmpty(DQueue *pqueue);
+void pushFront(DQueue *pqueue, int value);
+void PushRear(DQueue *pqueue, int value);
+void popFront(DQueue *pqueue);
+void PopRear(DQueue *pqueue);
+int getSize(DQueue *pqueue); 
+void print(DQueue *pqueue); 
 
 
 #endif
