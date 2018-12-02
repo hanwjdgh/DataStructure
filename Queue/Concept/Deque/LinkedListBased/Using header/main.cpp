47c7e60dde981e98f4b96ba52b1059627ed08576
hanwjdgh <hanwjdgh@naver.com>
2018-12-02 14:14:50 +0900

    main.cpp

--- Queue/Concept/Deque/LinkedListBased/Using header/main.cpp
+++ Queue/Concept/Deque/LinkedListBased/Using header/main.cpp
@@ -4,19 +4,30 @@
 
 using namespace std;
 
-int main(){
-    cin.tie(NULL);
+int main() {
+	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-    DQueue queue;
-    
-    init(&queue);
+	DQueue dqueue;
 
-    pushFront(&queue,1);
-    pushFront(&queue,2);
-    pushRear(&queue,3);
-    pushRear(&queue,4);
-    
-    return 0;
+	init(&dqueue);
+
+	pushFront(&dqueue, 2);
+	pushFront(&dqueue, 1);
+	pushRear(&dqueue, 3);
+	pushRear(&dqueue, 4);
+
+	print(&dqueue);
+
+	cout << "Size: " << getSize(&dqueue) << "\n";
+
+	cout << "Empty: " << (isEmpty(&dqueue) ? "TRUE" : "FALSE") << "\n";
+	
+	popFront(&dqueue);
+	popRear(&dqueue);
+
+	print(&dqueue);
+
+	return 0;
 }
